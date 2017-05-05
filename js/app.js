3879fdd5a9963fe489b69ec3d4ee5800f02be006
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-05-05 00:41:25 -0500

    creados botones footer, funcion scroll parcial

--- js/app.js
+++ js/app.js
@@ -2,29 +2,38 @@ var menu = document.getElementById("menu");
 var linkPrincipal = document.getElementById("principal");
 var secciones = document.getElementsByClassName("in-section");
 
+var portfolio = secciones[0];
+var about = secciones[1];
+var contact = secciones[2];
 
-function cabezalCambio() {
-    console.log(scrollY);
-    
-    if (window.scrollY > 80) {
-       menu.style.lineHeight = "30px";
-       linkPrincipal.style.fontSize = "20px";
-    } else if (window.scrollY < 80) {
+function menuScroll() {
+    var scrollCount = document.body.scrollTop;
+     console.log(scrollCount);
+    if (scrollCount > 60) {
+        menu.style.lineHeight = "30px";
+        linkPrincipal.style.fontSize = "20px";
+    } else if (scrollCount < 60) {
         menu.style.lineHeight = "60px";
         linkPrincipal.style.fontSize = "27px";
     }
-    
-    if (window.scrollY > 600 && window.scrollY < 1300) {
-        secciones[0].style.backgroundColor = "#18BC9C";
-    } else if (window.scrollY < 1310 && window.scrollY > 1300) {
-        secciones[0].style.backgroundColor = "rgba(24, 188, 156, 0)";
+
+   /* if (scrollCount > 600 && scrollCount < 1300) {
+        portfolio.setAttribute("class", "color");
+    } else {
+        portfolio.removeAttribute("class", "color");
     }
-    
-    if (window.scrollY > 1310 && window.scrollY < 2000) {
-        secciones[1].style.backgroundColor = "#18BC9C";
-    } else if (window.scrollY < 1310 && window.scrollY > 2000) {
-        secciones[1].style.backgroundColor = "rgba(24, 188, 156, 0)";
+
+    if (scrollCount > 1301 && scrollCount < 2000) {
+        about.setAttribute("class", "color");
+    } else {
+        about.removeAttribute("class", "color");
     }
+
+    if (scrollCount > 2001 && scrollCount < 2430) {
+        contact.setAttribute("class", "color");
+    } else {
+        contact.removeAttribute("class", "color");
+    }*/
 }
 
-document.addEventListener("scroll", cabezalCambio);
+document.addEventListener("scroll", menuScroll);
